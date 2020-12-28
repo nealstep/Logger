b185ad9674d7a6b7e971fb787f38a2b350edb5a7
Neal Stephenson <neal@bakerst.org>
2020-12-27 19:55:19 -0500

    initial code

---
+++ include/Logger.h
@@ -0,0 +1,44 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#define LOGGER_MSG_LEN 128
+#define LOGGER_NUM_LEN 16
+
+void send(const char* msg);
+
+enum logger_levels {
+    logger_none,
+    logger_error,
+    logger_warning,
+    logger_info,
+    logger_debug
+};
+
+class Logger {
+    public:
+        static Logger& get_instance() {
+            static Logger instance;
+            return instance;
+        }   
+        Logger(Logger const&) = delete;
+        void operator=(Logger const&) = delete;
+
+        void set_level(logger_levels new_level);
+        void error(const char* msg);
+        void info(const char* msg);
+        void debug(const char* msg);
+        void val(const char* tag, uint32_t val, bool hex=true, logger_levels log_level=logger_info);
+    protected:
+        Logger(void) {}
+        ~Logger(void) {}
+    private:
+        logger_levels level=logger_info;
+        static char message[LOGGER_MSG_LEN];
+        static char num_buf[LOGGER_NUM_LEN];
+
+        void finish(const char* mesg);
+};
+
+extern Logger& l;
+
+#endif // LOGGER_H
---
+++ src/Logger.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+
+#include "logger.h"
+
+Logger& l = Logger::get_instance();
+
+char Logger::message[LOGGER_MSG_LEN];
+char Logger::num_buf[LOGGER_NUM_LEN];
+
+static const char err_str[] = "! ";
+static const char inf_str[] = "* ";
+static const char val_str[] = "^ ";
+static const char sep_str[] = ":";
+static const char hex_str[] = "0x";
+
+void Logger::set_level(logger_levels new_level) {
+    level = new_level;
+}
+void Logger::error(const char* msg) {
+    if (level > logger_error) {
+        return;
+    }
+    strcpy(message, err_str);
+    finish(msg);
+}
+
+void Logger::info(const char* msg) {
+    if (level > logger_info) {
+        return;
+    }
+    strcpy(message, inf_str);
+    finish(msg);
+}
+
+void Logger::val(const char* tag, uint32_t val, bool hex, logger_levels log_level) {
+    size_t slen;
+    
+    if (level > log_level) {
+        return;
+    }
+    strcpy(message, val_str);
+    if (hex) {
+        ltoa(val, num_buf, 16);
+    } else {
+        ltoa(val, num_buf, 10);
+    }
+    slen = LOGGER_MSG_LEN-3;
+    strncat(message, tag, slen);
+    slen -= strlen(tag);
+    strncat(message, sep_str, slen);
+    slen -= 1;
+    if (hex) {
+      strncat(message, hex_str, slen);
+      slen -= 2;
+    }
+    strncat(message, num_buf, slen);
+    send(message);  
+}     
+
+void Logger::finish(const char* msg) {
+    strncat(message, msg, LOGGER_MSG_LEN-3);
+    send(message);  
+}
